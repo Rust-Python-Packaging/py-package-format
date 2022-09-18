87072f9b33ad5584b6a7536ba679466146142071
Allstreamer <48365544+Allstreamer@users.noreply.github.com>
2022-09-18 20:10:03 +0200

    Added some inital stuff

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,23 @@
+enum PythonTag {}
+
+enum ABITag {
+    None,
+}
+
+struct Platform {
+    OS: String,
+    Architecture: String,
+}
+
+struct WheelPackageName {
+    distribution: String,
+    version: String,
+    build_tag: String,
+    python_tag: String,
+    abi_tag: ABITag,
+    platform_tag: Platform,
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
